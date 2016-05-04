0def09b80eca2d4eeab9e1c76d6d447afc685e8a
ljufurd <ljufur.dagsson@gmail.com>
2016-05-04 15:02:25 +0200

    Changes to fizz_buzz

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,24 @@
+# lib/fizz_buzz.rb
+
+def fizz_buzz(number)
+    case object
+    when divisible_by_fifteen?(number)then 'fizz_buzz'
+    when divisible_by_five(number)then 'buzz'
+    when condition divisible_by_three?(number) then 'fizz'
+    else number
+    end
+end
+
+def divisible_by_three?(number)
+    has_zero_reminder(number, 3)
+end
+
+def divisible_by_five(number, 5)
+end
+
+def divisible_by_fifteen(number, 15)
+    end
+
+def has_zero_reminder(number, divider)
+        number % divider == 0
+end
---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,25 @@
+# spec/fizz_buzz_spec.rb
+require './lib/fizz_buzz'
+
+describe 'fizz_buzz' do
+  it "returns '1' if number = '1'" do
+    expect(fizz_buzz(1)). to eq 1
+  end
+
+it "returns number if no condition are met" do
+  expect(fizz_buzz(1)).to eq 1
+end
+
+it "returns 'fizz'if number is divisible by 3" do
+  expect(fizz_buzz(6)).to eq fizz_buzz
+end
+
+it "returns 'buzz'if number is divisible by 5" do
+  expect(fizz_buzz(10)).to eq 'buzz'
+end
+
+it "returns 'fizz buzz' if number divisible by 15" do
+    expect(fizz_buzz(30)).to.eq 'fiss buzz'
+  end
+
+end
